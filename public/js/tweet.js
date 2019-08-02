5d9406e410cc4524d304a2963465b051ac9fed5a
Divyanshu <divyanshu.r46956@gmail.com>
2019-08-02 22:13:23 +0200

    code cleanup. 👋

--- public/js/tweet.js
+++ public/js/tweet.js
@@ -1,23 +1,13 @@
- 
+$(document).ready(function () {
+	$("#btnTweet").click(function (e) {
+		const selectedText = $("#comment").val();
 
- $(document).ready(function () {
-	 $('#btnTweet').click(function (e) {
-	 
-	 var textToTweet = $('#comment').val();
-	 
-	 if (textToTweet.length > 140) {
-	 
-	 alert('Tweet should be less than 140 Chars');
-	 
-	 }
-	 else {
-	 
-			 var twtLink = 'http://twitter.com/home?status=' +encodeURIComponent(textToTweet);
-			 
-			 window.open(twtLink,'_blank');
-	 
-	 }
-
-	 });
- });
- 
+		if (selectedText.length > 140) {
+			alert("Tweet should be less than 140 character!");
+		} else {
+			const tweet =
+				"http://twitter.com/home?status=" + encodeURIComponent(selectedText);
+			window.open(tweet, "_blank");
+		}
+	});
+});
